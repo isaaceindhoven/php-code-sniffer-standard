a39b1101a858210d3a10c250cf150bb26b3eeb40
Jeroen Noten <jeroen.noten@isaac.nl>
2020-04-09 17:35:11 +0200

    Add sniff for multiple lines per use

---
+++ src/Standards/ISAAC/Sniffs/Namespaces/MultipleLinesPerUseSniff.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IsaacCodingStandard\Standards\ISAAC\Sniffs\Namespaces;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use SlevomatCodingStandard\Helpers\TokenHelper;
+use SlevomatCodingStandard\Helpers\UseStatementHelper;
+
+use const T_SEMICOLON;
+use const T_USE;
+
+class MultipleLinesPerUseSniff implements Sniff
+{
+    public const CODE_MULTIPLE_LINES_PER_USE = 'MultipleLinesPerUse';
+    public const ERROR_MESSAGE = 'Use statement "%s" should be placed on a single line.';
+
+    /**
+     * @return int[]
+     */
+    public function register(): array
+    {
+        return [T_USE];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(File $phpcsFile, $usePointer)
+    {
+        if (UseStatementHelper::isAnonymousFunctionUse($phpcsFile, $usePointer)) {
+            return;
+        }
+
+        $tokens = $phpcsFile->getTokens();
+
+        $semicolonPointer = TokenHelper::findNext($phpcsFile, T_SEMICOLON, $usePointer);
+        $fqn = UseStatementHelper::getFullyQualifiedTypeNameFromUse($phpcsFile, $usePointer);
+
+        if ($tokens[$usePointer]['line'] === $tokens[$semicolonPointer]['line']) {
+            return;
+        }
+
+        $phpcsFile->addError(
+            self::ERROR_MESSAGE,
+            $usePointer,
+            self::CODE_MULTIPLE_LINES_PER_USE,
+            [$fqn]
+        );
+    }
+}
